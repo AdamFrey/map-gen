86b400d954fad20a94cd664e9d18155c7450799c
Mitchell Hillman <mhillman@roomkey.com>
2017-12-12 09:13:30 -0500

    remove touch controls

--- script.js
+++ script.js
@@ -11,20 +11,6 @@ function Controls() {
   document.addEventListener('touchend', this.onTouchEnd.bind(this), false);
 }
 
-Controls.prototype.onTouch = function(e) {
-  var t = e.touches[0];
-  this.onTouchEnd(e);
-  if (t.pageY < window.innerHeight * 0.5) this.onKey(true, { keyCode: 38 });
-  else if (t.pageX < window.innerWidth * 0.5) this.onKey(true, { keyCode: 37 });
-  else if (t.pageY > window.innerWidth * 0.5) this.onKey(true, { keyCode: 39 });
-};
-
-Controls.prototype.onTouchEnd = function(e) {
-  this.states = { 'left': false, 'right': false, 'forward': false, 'backward': false };
-  e.preventDefault();
-  e.stopPropagation();
-};
-
 Controls.prototype.onKey = function(val, e) {
   var state = this.codes[e.keyCode];
   if (typeof state === 'undefined') return;
